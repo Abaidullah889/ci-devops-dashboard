04a422f0a0c497019cad431c3a27184acd07d7d1
Abaidullah Asif <ubaidtech889@gmail.com>
2025-07-23 03:12:38 +0200

    all errors are fixed and now testing with different tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,7 +3,7 @@ from app.main import add
 
 class TestAdd(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(add(3, 4), 9)
+        self.assertEqual(add(3, 4), 7)
 
 if __name__ == "__main__":
     unittest.main()
